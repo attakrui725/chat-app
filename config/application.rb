419de3d6549071aed57c8e55582a647606233e3e
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-11-09 11:19:10 +0900

    投稿時間の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module ChatApp
       g.javascripts false
       g.helper false
       g.test_framework false
+      
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+   
   end
 end
